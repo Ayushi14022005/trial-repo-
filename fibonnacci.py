ef7118ae395fcaac0a5952969b7e9246644110fd
prabhanjan27 <149321203+prabhanjan27@users.noreply.github.com>
2023-11-09 15:53:41 +0530

    Update fibonnacci.py

--- fibonnacci.py
+++ fibonnacci.py
@@ -9,3 +9,14 @@ def fibonacci(n):
         return [0]
     elif n == 2:
         return fib_sequence
+ # Generate the Fibonacci sequence up to the nth term
+    for i in range(2, n):
+        next_term = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_term)
+
+    return fib_sequence
+
+# Get the first 10 terms of the Fibonacci sequence
+n = 10
+result = fibonacci(n)
+print(result)
